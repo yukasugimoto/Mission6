46e1101fd3611f94e660ec39f03035585cc363d5
YukaSugimoto <43984057+yukasugimoto@users.noreply.github.com>
2018-12-27 15:39:14 +0900

    Update post_view.php

--- post_view.php
+++ post_view.php
@@ -1,4 +1,6 @@
 <?php
+//投稿のページ
+
 //セッション
 session_start();
 if(!isset($_SESSION["name"])) {
@@ -27,7 +29,7 @@ try{
 	
 	$sql = "SELECT * FROM Review WHERE image = :target;";
 	$stmt = $pdo->prepare($sql);
-    $stmt -> bindValue(":target", $target, PDO::PARAM_STR);
+   	$stmt -> bindValue(":target", $target, PDO::PARAM_STR);
 	$stmt -> execute();
 	$row = $stmt -> fetch(PDO::FETCH_ASSOC);
 	header("Content-Type: ".$mime[$row["ext"]] );
@@ -41,8 +43,14 @@ if( isset($_GET['id'])){
 	$stmt->execute($params);
 	$result  = $stmt->fetch(PDO::FETCH_ASSOC);
 
+	//おすすめ評価
+	if( $result['star'] === "1" ) { $star = "★☆☆☆☆" ;}
+	if( $result['star'] === "2" ) { $star = "★★☆☆☆" ;}
+	if( $result['star'] === "3" ) { $star = "★★★☆☆" ;}
+	if( $result['star'] === "4" ) { $star = "★★★★☆" ;}
+	if( $result['star'] === "5" ) { $star = "★★★★★" ;}
 
-	$pass = $_POST['pass'];
+$pass = $_POST['pass'];
 if( isset($_POST['edit'])){
 	$sql = "SELECT * FROM Review WHERE id=".$post_id;
 	$result2 = $pdo->query($sql);
@@ -81,30 +89,74 @@ if( isset($_POST['delete'])){
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<title>Review post</title>
+		<style>
+			body{
+				background: #ffffff;
+			}
+			header{
+				text-align: center;			
+			}
+			.main{
+				background: #ffffff;
+				max-width: 700px;
+				padding: 10px;
+				padding-bottom:60px;
+				text-align: center;
+				border: 1px solid #cccccc;
+				margin: 30px auto;
+			}
+			.text{
+				display: inline-block;
+				text-align: left;
+			}
+			img{
+				width: 40%;
+			}
+			footer{
+				text-align: center;
+			}
+			.foot{
+				font-size: 10px;
+			}
+			
+		</style>
+
 	</head>
 	<body>
-		<h1>投稿内容</h1>
+		<header>
+			<h1><img src="table/logo.jpg"></h1>
+		</header>
+		<div class="main">
+		<br>
 		<br>
 		<?php if($result['ext'] == "jpg" || $result['ext'] == "JPG" || $result['ext'] == "png" || $result['ext'] == "PNG") : ?>
-			<img src="picture.php?target=<?php echo $result['image']; ?>" width="30%">
+			<img src="picture.php?target=<?php echo $result['image']; ?>" width="50%">
 		<?php elseif($result['ext'] == "mp4" || $result['ext'] == "MP4") : ?>
-			<video src="picture.php?target=<?php echo $result['image']; ?>" width="30%" controls></video>
+			<video src="picture.php?target=<?php echo $result['image']; ?>" width="50%" controls></video>
 		<?php endif; ?>
 		<br>
+		<p class="text">
 		日時：<?php echo $result['date']; ?> <br>
 		名前：<?php echo $result['name']; ?> <br>
 		場所：<?php echo $result['place']; ?> <br>
-		<?php echo $result['post']; ?> <br>
+		<?php echo $star; ?> <br><br>
+		<?php echo nl2br($result['post']); ?> <br>
 		<br><br>
+		</p>
 		<form action="" method="post">
-			<input type="text" name="pass" size="15" placeholder="パスワード">
+			<input type="password" name="pass" size="15" placeholder="パスワード">
 			<input type="submit" name="edit" value="編集">
 			<input type="submit" name="delete" value="削除">
 		</form>
 		<br>
 		<?php echo $er; ?>
 		<br>
-		<a href="review_top.php">戻る</a>
-		<br>
+		</div>
+		<footer>
+			<a href="review_top.php">戻る</a>
+			<br>
+			<p class="foot">Copyright 2018 YS</p>
+			<br>
+		</footer>
 	</body>
 </html>
