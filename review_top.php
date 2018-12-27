52086ff31b461b42a72a58236d7ef8b216d406e8
YukaSugimoto <43984057+yukasugimoto@users.noreply.github.com>
2018-12-27 16:01:07 +0900

    Update review_top.php

--- review_top.php
+++ review_top.php
@@ -1,11 +1,14 @@
 <?php
+//トップ
+
 session_start();
 if(!isset($_SESSION["name"])) {
     $no_login_url = "login.php";
     header("Location: {$no_login_url}");
     exit;
 }
-		//データベース接続
+
+//データベース接続
 $dsn  =  '';
 $user  =  '';
 $password  =  '';
@@ -30,10 +33,40 @@ $password  =  '';
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<title>Review top</title>
-		<link rel="stylesheet" href="review_top.css">
+		<style>
+			body{
+				background: #ffffff;
+				font-family: Meiryo;
+			}
+			header{
+				background: #ffffff;
+				text-align: center;
+			}
+			.main{
+				background: #ffffff;
+				max-width: 700px;
+				padding: 10px;
+				padding-bottom: 60px;
+				text-align: center;
+				border: 1px solid #cccccc;
+				margin: 30px auto;
+			}
+	
+
+			footer{
+				font-size: 10px;
+				text-align: center;
+			}
+				
+		</style>
 	</head>
 	<body>
-		<h1>旅行の写真をシェアしよう</h1>
+		<header>
+			<h1><img src="table/logo.jpg"></h1>
+		</header>
+		<div class="main">
+		<br>
+		<h2>旅行の写真をシェアしよう</h2>
 		<br>
 		投稿は<a href="review_post.php">コチラ</a> <br>
 		<br>
@@ -45,14 +78,21 @@ $password  =  '';
 		<br>
 		<?php foreach ($result as $row): ?>
 			<?php if ($row['ext'] == "jpg" || $row['ext'] == "JPG" || $row['ext'] == "png" || $row['ext'] == "PNG") : ?>
-				<a href="post_view.php?id=<?php echo $row['id']; ?>"><img src="picture.php?target=<?php echo $row['image']; ?>"></a>
+				<a href="post_view.php?id=<?php echo $row['id']; ?>"><img src="picture.php?target=<?php echo $row['image']; ?>" width="200" height="150"></a>
 			<?php elseif($row['ext'] == "mp4" || $row['ext'] == "MP4") : ?>
 				<a href="post_view.php?id=<?php echo $row['id']; ?>"><video src="picture.php?target=<?php echo $row['image']; ?>" width="200" height="150"></video></a>
 			<?php endif; ?>
 		<?php endforeach; ?>
 		<br><br>
-		<a href="logout.php?logout">ログアウト</a>
+		過去の投稿を見る<a href="review_log.php">→</a>
+		<br><br>
 		<br>
-
+		</div>
+		<footer>
+			<a href="logout.php?logout">ログアウト</a>
+			<br>
+			Copyright 2018 YS
+			<br>
+		</footer>
 	</body>
 </html>
